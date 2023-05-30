4e6a89e5a4b8a4730c6334fddbf4f452a1a2bd4d
joshua <joshuasajeev103@gmail.com>
2023-05-30 12:01:42 +0530

    cleaning

--- pcconfig.py
+++ pcconfig.py
@@ -1,8 +1,5 @@
 import pynecone as pc
 
-class AppConfig(pc.Config):
-    pass
-
 config = pc.Config(
     app_name="app",
     db_url="mysql+mysqlconnector://root:joshua@localhost:3306/PasswordManager",
